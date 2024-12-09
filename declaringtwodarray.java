de9bd321a1e5c8460fa1d7ccb3e513b3f88a4fd6
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-09 15:41:54 +0530

    Add files via upload

---
+++ declaringtwodarray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class declaringtwodarray{
+    public static void main(String args[]){
+        int a[][]={{1,2},{3,4}};
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
